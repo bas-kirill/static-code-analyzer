d1f13605b0c4ab855bc0df6399bc23b2868f710a
eertree <baskirill.an@gmail.com>
2023-01-22 19:35:35 +0200

    add tests

---
+++ test/test_0.py
@@ -0,0 +1,8 @@
+if True:
+
+
+
+
+ print('hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'); #TODO
+ print('hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'); #TODO
+
